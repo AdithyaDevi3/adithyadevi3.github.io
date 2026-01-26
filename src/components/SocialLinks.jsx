bb681e992529c72e28d957e565701a862cad0cde
Adithya Devi <adithya.r.devi@gmail.com>
2026-01-25 23:29:24 -0500

    updates

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,3 +1,5 @@
+import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
+
 function SocialLinks() {
   return (
     <div className="fixed bottom-8 left-8 flex gap-4 z-50 pointer-events-auto">
@@ -7,23 +9,24 @@ function SocialLinks() {
         rel="noopener noreferrer"
         className="bg-black/40 backdrop-blur-sm text-cyan-300 hover:text-white hover:bg-black/60 border border-cyan-500/30 px-4 py-2 rounded-lg transition-all"
       >
-        GitHub
+        <FaGithub size={24}/>
       </a>
 
       <a
-        href="https://linkedin.com/in/yourusername"
+        href="https://www.linkedin.com/in/adithya-devi"
         target="_blank"
         rel="noopener noreferrer"
-        className="bg-black/40 backdrop-blur-sm text-cyan-300 hover:text-white hover:bg-black/60 border border-cyan-500/30 px-4 py-2 rounded-lg transition-all"
+        className="bg-black/40 backdrop-blur-sm text-cyan-300 hover:text-white hover:bg-black/60 border border-cyan-500/30 px-4 py-2 rounded-lg flex items-center gap-2 transition-all"
       >
-        LinkedIn
+        <FaLinkedin size={24} />
+      
       </a>
 
       <a
         href="mailto:your.email@example.com"
         className="bg-black/40 backdrop-blur-sm text-cyan-300 hover:text-white hover:bg-black/60 border border-cyan-500/30 px-4 py-2 rounded-lg transition-all"
       >
-        Email
+        <FaEnvelope size={24}/>
       </a>
     </div>
   );
